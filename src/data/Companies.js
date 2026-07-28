a04562d4f70a6e00e70a76ca4a61deac3441a53a
gaurika-24 <gaurikachandna248@gmail.com>
2026-07-29 05:21:58 +0530

    Added the list of companies

---
+++ src/data/Companies.js
@@ -0,0 +1,118 @@
+export const companies = [
+  // FAANG / Big Tech
+  "Amazon",
+  "Apple",
+  "Google",
+  "Meta",
+  "Microsoft",
+  "Netflix",
+  "NVIDIA",
+  "Intel",
+  "AMD",
+  "Oracle",
+  "IBM",
+  "Adobe",
+  "Salesforce",
+  "Cisco",
+  "SAP",
+  "Dell",
+  "HP",
+  "Qualcomm",
+  "Broadcom",
+
+  // Indian IT & Consulting
+  "TCS",
+  "Infosys",
+  "Wipro",
+  "HCLTech",
+  "Tech Mahindra",
+  "LTIMindtree",
+  "Persistent Systems",
+  "Mphasis",
+  "Coforge",
+  "Hexaware",
+  "L&T Technology Services",
+  "KPIT Technologies",
+  "Birlasoft",
+  "Zensar Technologies",
+
+  // Consulting
+  "Accenture",
+  "Deloitte",
+  "PwC",
+  "EY",
+  "KPMG",
+  "Capgemini",
+  "Cognizant",
+
+  // Product Companies
+  "Atlassian",
+  "Intuit",
+  "ServiceNow",
+  "VMware",
+  "Workday",
+  "Snowflake",
+  "Databricks",
+  "MongoDB",
+  "Redis",
+  "Elastic",
+
+  // E-commerce
+  "Flipkart",
+  "Meesho",
+  "Myntra",
+  "Nykaa",
+  "Blinkit",
+  "Zepto",
+  "Swiggy",
+  "Zomato",
+
+  // FinTech
+  "Paytm",
+  "PhonePe",
+  "Razorpay",
+  "CRED",
+  "Groww",
+  "Upstox",
+  "Zerodha",
+
+  // Startups
+  "Physics Wallah",
+  "Unacademy",
+  "Urban Company",
+  "ShareChat",
+  "Dream11",
+  "Freshworks",
+  "BrowserStack",
+  "Postman",
+
+  // Telecom
+  "Jio",
+  "Airtel",
+  "Vodafone Idea",
+
+  // Semiconductor
+  "Texas Instruments",
+  "Micron",
+  "Applied Materials",
+
+  // Automobile
+  "Tesla",
+  "Tata Motors",
+  "Mahindra",
+  "Maruti Suzuki",
+
+  // Banking
+  "HDFC Bank",
+  "ICICI Bank",
+  "Axis Bank",
+  "State Bank of India",
+
+  // Miscellaneous
+  "Siemens",
+  "Bosch",
+  "Shell",
+  "GE Aerospace",
+  "Boeing",
+  "Airbus"
+];
